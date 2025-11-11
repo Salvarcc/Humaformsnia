5c8e452efa8d4048f12354cd1b5628df2f950c96
Salvador Rivera <salvadorandreriverachavez@gmail.com>
2025-11-10 22:10:22 -0500

    Tamaño de la pantalla

--- Humaformsnia/MyForm.h
+++ Humaformsnia/MyForm.h
@@ -49,7 +49,7 @@ namespace Humaformsnia {
 		void InitializeComponent(void)
 		{
 			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
+			this->Size = System::Drawing::Size(1400,700);
 			this->Text = L"MyForm";
 			this->Padding = System::Windows::Forms::Padding(0);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
