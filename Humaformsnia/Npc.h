c68828063caca36f6892951f0557ec5a67a5ce48
SamuelR2107 <samirodri2017@gmail.com>
2025-11-10 23:03:38 -0500

    CLASS NPC NUEVO

--- Humaformsnia/Npc.h
+++ Humaformsnia/Npc.h
@@ -1,1 +1,21 @@
 #pragma once
+
+class NPC : public Entidad
+{
+public:
+	NPC();
+	~NPC();
+	void moverse(Direccion num);
+	void moversprite(Direccion num);
+
+	int getVidas();
+
+	void setVidas(int vidas);
+
+private:
+
+	int vidas;
+
+};
+
+// el que lee es perra
