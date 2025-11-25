90be409938be48b9291f871f27f807186763680b
Shayli Mishelly Flores Urbano <floresshayli04@gmail.com>
2025-11-25 11:03:09 -0500

    .

--- Humaformsnia/MyForm.h
+++ Humaformsnia/MyForm.h
@@ -17,7 +17,7 @@ namespace Humaformsnia {
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
-	using namespace System::Windows::Forms;
+	using namespace System::Windows::Forms; 
 	using namespace System::Data;
 	using namespace System::Drawing;
 	using namespace System::Collections::Generic;
